6545dce8bae1a6e43638e18dba1355de0e8e1d20
unknown <akbiyik.ilker7@gmail.com>
2022-05-05 15:03:54 +0200

    basic routing & images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { CreateOrderComponent } from './create-order/create-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    NavbarComponent,
+    AboutComponent,
+    CreateOrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'services', component: ServicesComponent},
+      {path: 'about', component: AboutComponent},
+      {path: 'create-order', component: CreateOrderComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
