d6dd0141fb2ced139fefa816b16df6bc6f4b0e2b
unknown <akbiyik.ilker7@gmail.com>
2022-05-11 08:44:48 +0200

    store test works

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as myActions from '../actions/index'
 
 @Component({
   selector: 'app-story',
@@ -7,13 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./story.component.css']
 })
 export class StoryComponent implements OnInit {
+  totalPrice$: Observable<number>;
+  names$: Observable<string[]>;
 
-  constructor() { }
+  constructor(private store: Store<{totalPrice:number, names:string[]}>) {
+    this.totalPrice$ = store.select('totalPrice');
+    this.names$ = store.select('names');
+  }
 
   test(): void {
     console.log("clicked on button")
-    
+    this.store.dispatch(myActions.test())
   }
+
 
   ngOnInit(): void {
   }
